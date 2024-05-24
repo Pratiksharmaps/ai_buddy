9c6f978d5c00697a06a650d2c0f25ca6a8435e10
Pratiksharmaps <122214913+Pratiksharmaps@users.noreply.github.com>
2024-05-24 22:09:13 +0530

    final

--- lib/pages/Chat.dart
+++ lib/pages/Chat.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
-
-import 'package:ai_buddy/data/remote/api/Conifiguration.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:ai_buddy/utils/utils.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
